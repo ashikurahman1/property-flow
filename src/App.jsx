42ddbb8ef2224abfe9891947adeefe2ab05442a6
ashikurahman1 <ashikurahmantuhin@gmail.com>
2025-11-30 23:11:01 +0600

    add: header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <>
-      <h1>App</h1>
+      <Header />
       <Outlet />
     </>
   );
